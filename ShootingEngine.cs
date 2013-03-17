50df5661bc941c1df83d19bc9888a2fe49ba5e35
Velko Nikolov <velko.nikolov@gmail.com>
2013-03-17 20:58:50 +0200

    ShootingEngine.cs

---
+++ ShootingEngine.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AcademyPopcorn
+{
+    /* problem 13 */
+    public class ShootingEngine : Engine
+    {
+
+        ShootingRacket racket;
+
+        public override void AddObject(GameObject obj) {
+            var as_racket = obj as ShootingRacket;
+            if (as_racket != null) {
+                racket = as_racket;
+            }
+            base.AddObject(obj);
+        }
+        
+        public ShootingEngine(IRenderer renderer, IUserInterface userInterface)
+            : base(renderer, userInterface) {
+        }
+
+        public ShootingEngine(IRenderer renderer, IUserInterface userInterface, int sleep_time)
+            : base(renderer, userInterface, sleep_time) {
+        }
+
+        public void ShootPlayerRacket() {
+            var local = racket;
+            if (local != null && local.CanShoot) {
+                local.Shoot();
+            }
+        }
+
+
+    }
+}
