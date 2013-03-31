701ee6e3e729091b6678f3cacc2be58a344f7acd
Velko Nikolov <velko.nikolov@gmail.com>
2013-04-01 00:30:24 +0300

    2013-04-01-00-30-23

---
+++ js-competition-scaffold/problem.js
@@ -0,0 +1,22 @@
+
+
+var Solve = function(args) {
+
+    var ii,jj;
+    var ret = "";
+    var line = function(str) { ret += str + "\r\n"; }
+    
+    var n = parseInt(args.shift(), 10);
+    
+    return ret;
+    
+};
+
+
+var test1 = [];
+var tests = [test1];
+
+
+
+
+
