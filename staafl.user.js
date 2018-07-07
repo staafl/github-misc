d3d5486a3f7a28dae5f0e1de75f109d64937b41b
Velko Nikolov <velko.nikolov@gmail.com>
2018-07-07 16:18:20 +0300

    staafl.user.js - grant

--- staafl.user.js
+++ staafl.user.js
@@ -5,7 +5,22 @@
 // @description  try to take over the world!
 // @author       staafl
 // @match        http*://*/*
-// @grant        none
+// @grant unsafeWindow
+// @grant GM_addStyle
+// @grant GM_getValue
+// @grant GM_setValue
+// @grant GM_xmlhttpRequest
+// @grant GM_registerMenuCommand
+// @grant GM_deleteValue
+// @grant GM_listValues
+// @grant GM_getResourceText
+// @grant GM_getResourceURL
+// @grant GM_log
+// @grant GM_openInTab
+// @grant GM_setClipboard
+// @grant GM_info
+// @grant GM_getMetadata
+// @run-at document-start
 // @download     https://github.com/staafl/github-misc/raw/master/staafl.user.js
 // ==/UserScript==
 
