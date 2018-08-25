5fa5e05e4ce9a2546f9662bb3ff1ebf13fc92a57
Velko Nikolov <velko.nikolov@gmail.com>
2018-08-25 21:55:01 +0300

    same

--- staafl.user.js
+++ staafl.user.js
@@ -69,6 +69,9 @@
                 console.log("down");
                 should = false;
             }
+            else {
+                console.log(evtobj);
+            }
         }
 
         function detectCtrlUp(e){
