109509221b69ff35ae230c50316139c11a7903d4
Velko Nikolov <velko.nikolov@tick42.com>
2018-12-29 22:19:00 +0200

    same

--- staafl.user.js
+++ staafl.user.js
@@ -424,17 +424,13 @@ const debug = true;
 
             function doIt() {
               if (debug) { console.log("doIt() called..."); }
-              var resultLinks = $x("//a[@onmousedown]", XPathResult.ORDERED_NODE_SNAPSHOT_TYPE);
-              resultLinks.forEach(function(link) {  // loop over links
+              const resultLinks = document.getElementsByTagName("a");
+              console.log(resultLinks);
+              resultLinks.forEach(function(link) {
+                var oldLink = link.href;
                 if (link.getAttribute('onmousedown')) {
                   link.removeAttribute('onmousedown');
                 }
-              });
-
-              resultLinks = $x("//a");
-              console.log(resultLinks);
-              resultLinks.forEach(function(link) {  // loop over links
-                var oldLink = link.href;
                 if (isGoogle) {
                   console.log(oldLink);
                   var matches = /url\?(url|q)=(.+?)&/.exec(oldLink);
@@ -455,60 +451,6 @@ const debug = true;
                     link.href = link.href.replace(/(www[.])?reddit[.]com/, "old.reddit.com");
                 }
               });
-            }
-
-
-            // XPath helper, from
-            // https://wiki.greasespot.net/XPath_Helper
-            function $x() {
-              var x='';
-              var node=document;
-              var type=0;
-              var fix=true;
-              var i=0;
-              var cur;
-
-              function toArray(xp) {
-                var final=[], next;
-                while (next=xp.iterateNext()) {
-                  final.push(next);
-                }
-                return final;
-              }
-
-              while (cur=arguments[i++]) {
-                switch (typeof cur) {
-                  case "string": x+=(x=='') ? cur : " | " + cur; continue;
-                  case "number": type=cur; continue;
-                  case "object": node=cur; continue;
-                  case "boolean": fix=cur; continue;
-                }
-              }
-
-              if (fix) {
-                if (type==6) type=4;
-                if (type==7) type=5;
-              }
-
-              // selection mistake helper
-              if (!/^\//.test(x)) x="//"+x;
-
-              // context mistake helper
-              if (node!=document && !/^\./.test(x)) x="."+x;
-
-              var result=document.evaluate(x, node, null, type, null);
-              if (fix) {
-                // automatically return special type
-                switch (type) {
-                  case 1: return result.numberValue;
-                  case 2: return result.stringValue;
-                  case 3: return result.booleanValue;
-                  case 8:
-                  case 9: return result.singleNodeValue;
-                }
-              }
-
-              return fix ? toArray(result) : result;
             }
         }
 
