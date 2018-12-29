f7b0837d0624da9e16f356a9ace746fa313b5116
Velko Nikolov <velko.nikolov@tick42.com>
2018-12-29 21:46:19 +0200

    same

--- staafl.user.js
+++ staafl.user.js
@@ -393,7 +393,6 @@ const debug = true;
                     (mutation.target.nodeName == 'DIV' && namedItem && namedItem.value == 'taw'))) {
                   should = true;
                 } else if (~window.location.href.indexOf("facebook")) {
-                    console.log("there");
                     console.log(mutation.target.nodeName, namedItem && namedItem.value);
                     if (mutation.target.nodeName == 'A' ||
                         (namedItem && namedItem.value == "content")) {
