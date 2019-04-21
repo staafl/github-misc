9a373cf1bd72ad8eaa94830f13e4964867c391e4
Velko Nikolov <velko.nikolov@tick42.com>
2019-04-21 10:18:46 +0300

    same
    [master]

--- staafl.user.js
+++ staafl.user.js
@@ -63,7 +63,7 @@ const debug = true;
 
     function doActualStuff() {
 // inc:: version: ["](.*?)["] => version: "#{$1+1}"
-        unsafeWindow.staafl = { version: "55"};
+        unsafeWindow.staafl = { version: "56"};
 
         if (debug) {
             console.log("Staafl userscript version " + unsafeWindow.staafl.version);
@@ -439,7 +439,7 @@ const debug = true;
 
         function udemyStretch() {
             var interval = setInterval(function() {
-                var toStretch = document.querySelectorAll('div[class^="curriculum-item-view--content-container-"] > div:nth-child(1)')[0];
+                var toStretch = document.querySelectorAll('div[class^="app--curriculum-item--theatre-"] > div:nth-child(1)')[0];
                 if (toStretch) {
                     toStretch.style.maxHeight = "1000px"
                     clearInterval(interval);
